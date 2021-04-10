47cd6aea96b6cd792b49c5170a1ea50f977e59e1
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-10 14:12:39 +0100

    added article controller

---
+++ server/java_news_server/src/main/java/com/example/codeclan/java_news_server/controllers/ArticleController.java
@@ -0,0 +1,48 @@
+package com.example.codeclan.java_news_server.controllers;
+
+
+import com.example.codeclan.java_news_server.models.Article;
+import com.example.codeclan.java_news_server.repositories.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ArticleController {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @GetMapping(value="/articles")
+    public ResponseEntity<List<Article>> getAllArticles(){
+        return new ResponseEntity<>(articleRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/articles/{id}")
+    public ResponseEntity getArticle(@PathVariable Long id){
+        return new ResponseEntity<>(articleRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/articles")
+    public ResponseEntity<Article> postArticle(@RequestBody Article article){
+        articleRepository.save(article);
+        return new ResponseEntity<>(article, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value="/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@RequestBody Article article){
+        articleRepository.save(article);
+        return new ResponseEntity<>(article, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/articles/{id}")
+    public ResponseEntity<Article> deleteArticle(@PathVariable Long id){
+        Article found = articleRepository.getOne(id);
+        articleRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+}
+
