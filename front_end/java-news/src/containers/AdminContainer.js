fb5fb82aa03c1e252ba2ff57b37ed58f6f27d5d1
Jennifer Wardhaugh <jenwardhaugh@outlook.com>
2021-04-10 13:56:45 +0100

    AdminContainer added

---
+++ front_end/java-news/src/containers/AdminContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import JournalistForm from '../components/admin/JournalistForm';
+import Request from '../helpers/request';
+
+const AdminContainer = () => {
+
+    
+
+}
+
+export default AdminContainer;
