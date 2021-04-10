e7a570e173dc99828d864f9049ac56b288e56c5e
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-10 11:53:29 +0100

    created spring server

---
+++ server/java_news_server/src/main/java/com/example/codeclan/java_news_server/JavaNewsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.java_news_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNewsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNewsServerApplication.class, args);
+	}
+
+}
---
+++ server/java_news_server/src/test/java/com/example/codeclan/java_news_server/JavaNewsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.java_news_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNewsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
