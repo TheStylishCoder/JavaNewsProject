dbd5fe82d6c867302fc798a68380d09ae51a7cf4
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-10 19:14:25 +0100

    modified EditArticleList

--- front_end/java-news/src/components/admin/EditArticleList.js
+++ front_end/java-news/src/components/admin/EditArticleList.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const EditArticleList = () => {
+const EditArticleList = ({allArticles}) => {
+
+    const articleListItems = allArticles.map((article, index) => {
+        return(
+            <>
+                <li key={index}>
+                    {article.headline}
+                    <img src={article.image} height="300px" width="auto" />
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </li>
+            </>
+        )
+    })
 
     return(
         <>
+        <h2>Current Articles</h2>
+        <ul>
+            {articleListItems}
+        </ul>
         </>
     )
 
