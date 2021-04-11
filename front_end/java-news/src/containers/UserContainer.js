c75b9d66c5ecf4085ae6920963b841fb77b24896
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-11 22:00:17 +0100

    added routes to user container

--- front_end/java-news/src/containers/UserContainer.js
+++ front_end/java-news/src/containers/UserContainer.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import UserNavBar from '../components/users/UserNavBar';
+import UserProfile from '../components/users/UserProfile';
+import ReadingList from '../components/users/ReadingList';
+import RecentlyViewed from '../components/users/RecentlyViewed';
+import LocalNews from '../components/users/LocalNews';
 
 const UserContainer = () => {
 
     return(
         <>
+        <UserNavBar />
+
+        <Route exact path='/users/profile' render={() => {
+            return <UserProfile />
+        }} />
+
+        <Route exact path='/users/readinglist' render={() => {
+            return <ReadingList />
+        }} />
+
+        <Route exact path='/users/recentlyviewed' render={() => {
+            return <RecentlyViewed/>
+        }} />
+
+        <Route exact path='/users/localnews' render={() => {
+            return <LocalNews />
+        }} />
+
         </>
     )
 
