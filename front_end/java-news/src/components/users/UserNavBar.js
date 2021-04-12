1859d7c38d05f7677f50a7099f8d8844b4d754c0
Anna Cunningham <ac232@hotmail.co.uk>
2021-04-12 14:28:19 +0100

    user nav bar links setup

--- front_end/java-news/src/components/users/UserNavBar.js
+++ front_end/java-news/src/components/users/UserNavBar.js
@@ -4,6 +4,20 @@ const UserNavBar = () => {
 
     return(
         <>
+        <ul>
+            <li className="navLink">
+                <Link to="/users/profile">Profile</Link>
+            </li>
+            <li className="navLink">
+                <Link to="/users/readinglist">Reading List</Link>
+            </li>
+            <li className="navLink">
+                <Link to="/users/recentlyviewed">Recently Viewed</Link>
+            </li>
+            <li className="navLink">
+                <Link to="/users/localnews">News In My Area</Link>
+            </li>
+        </ul>
         </>
     )
 
